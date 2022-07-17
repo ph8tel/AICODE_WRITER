2e8e11b46bbe3af49b02d6bb6fcc6dede521b0c4
Joe Jackson <ph8tel@gmail.com>
2022-07-16 20:46:17 -0700

    update ui

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,7 @@
 
 import openai
 from pywebio import start_server
-from pywebio.output import put_table
+from pywebio.output import put_table, put_code
 from pywebio.input import input
 import os
 
@@ -17,14 +17,14 @@ def openai_response(question):
     )
     print(response.choices)
     print(response, '{}'.format(response.choices[0].text[6:]))
-    return '{}'.format(response.choices[0].text[6:])
-
+    # return '{}'.format(response.choices[0].text[6:])
+    return response.choices[0].text
 def main():
     while True:
         question = input('Describe your desired code')
         put_table([
             ['Q:', question],
-            ['A:', openai_response(question)]
+            ['A:', put_code(openai_response(question))]
         ])
 
 if __name__ == '__main__':
