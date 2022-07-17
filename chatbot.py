0d95794cae2d703d2fc2c3d66212ad01a85f3677
Joe Jackson <ph8tel@gmail.com>
2022-07-16 20:19:29 -0700

    staged for Heroku deploy

---
+++ chatbot.py
@@ -0,0 +1,32 @@
+
+import openai
+from pywebio import start_server
+from pywebio.output import put_table
+from pywebio.input import input
+import os
+
+openai.api_key = os.environ.get("OPENAI_API_KEY")
+
+def openai_response(question):
+
+    response = openai.Completion.create(
+        model="text-davinci-002",
+        prompt=question,
+        temperature=0.8,
+        max_tokens=1000,
+    )
+    print(response.choices)
+    print(response, '{}'.format(response.choices[0].text[6:]))
+    return '{}'.format(response.choices[0].text[6:])
+
+def main():
+    while True:
+        question = input('Describe your desired code')
+        put_table([
+            ['Q:', question],
+            ['A:', openai_response(question)]
+        ])
+
+if __name__ == '__main__':
+    port = os.environ.get("PORT", 8080)
+    start_server(main, port=port, debug=True)
